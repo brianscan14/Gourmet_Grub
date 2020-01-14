84b08713c15b6c951983abc329d6b67717664645
brianscan14 <brianscanlon14@gmail.com>
2020-01-14 20:16:25 +0000

    establishing connection with mongo DB as test, also added additional functionality from flask, added html file as template to render fo the test

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from os import path
@@ -14,8 +14,9 @@ app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 mongo = PyMongo(app)
 
 @app.route('/')
-def hello_test():
-    return 'Testing app...'
+@app.route('/get_recipies')
+def get_recipies():
+    return render_template("recipies.html", recipies=mongo.db.recipies.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
