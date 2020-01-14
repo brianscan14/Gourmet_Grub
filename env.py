8e72face7d805230838b8af11e06f57aa45072de
brianscan14 <brianscanlon14@gmail.com>
2020-01-14 18:49:19 +0000

    trying out different username, env var not working properly

--- env.py
+++ env.py
@@ -1,3 +1,3 @@
 import os
 
-os.environ["MONGO_URI"] = "mongodb+srv://floppy12:fairy1dairy432@mefirstcluster-rev2x.mongodb.net/good_grubDB?retryWrites=true&w=majority"
+os.environ["MONGO_URI"] = "mongodb+srv://spogman:Coppinger121@mefirstcluster-rev2x.mongodb.net/good_grubDB?retryWrites=true&w=majority"
