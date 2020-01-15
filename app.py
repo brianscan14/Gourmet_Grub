02f01278fc77545ac1aed168a093826e9fa954e0
brianscan14 <brianscanlon14@gmail.com>
2020-01-15 15:08:38 +0000

    imported regular expression library to use for string match in my search bar, then added route which takes query value from form or search bar, converts these to lower case so theres no silly errors and finds a matching string in the recipe name, any match of string for now

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import re
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -17,6 +18,14 @@ mongo = PyMongo(app)
 @app.route('/get_recipies')
 def get_recipies():
     return render_template("recipies.html", recipies=mongo.db.recipies.find())
+
+
+@app.route('/search_recipe')
+def search_recipe():
+    rec_search_query = request.args['query']
+    query = {'$regex': re.compile('.*{}.*'.format(rec_search_query), re.IGNORECASE)}
+    results = mongo.db.recipies.find({'recipe_name': query})
+    return render_template('search_recipe.html', query=rec_search_query, results=results)
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
