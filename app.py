2d744804da9bbb156488bbb43fc491b6c383493e
brianscan14 <brianscanlon14@gmail.com>
2020-01-15 17:02:19 +0000

    adding route to edit recipes if id match is found

--- app.py
+++ app.py
@@ -37,6 +37,12 @@ def insert_recipe():
     recipies.insert_one(request.form.to_dict())
     return redirect(url_for('get_recipies'))
 
+@app.route('/edit_recipe/<recipe_id>') 
+def edit_recipe(recipe_id): 
+    the_recipe =  mongo.db.recipies.find_one({"_id": ObjectId(recipe_id)}) 
+    all_cuisines =  mongo.db.cuisine.find()
+    return render_template('edit_recipe.html', recipe=the_recipe, cuisines=all_cuisines) 
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
     port=int(os.environ.get('PORT')),
