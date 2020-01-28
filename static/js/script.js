b0fbd6b616b8f804b9c54abb2a55e94f7a82b27d
brianscan14 <brianscanlon14@gmail.com>
2020-01-28 14:45:01 +0000

    testing put form where u can add steps to recipe instructions using jquery

--- static/js/script.js
+++ static/js/script.js
@@ -11,3 +11,28 @@ jQuery(document).ready(function($) {
     textBox.setSelectionRange(start, end);
   });
 });
+
+// testing the add form function
+
+$(document).ready(function() {
+    var max_fields      = 10;
+    var wrapper         = $(".container1");
+    var add_button      = $(".add_form_field");
+ 
+    var x = 1;
+    $(add_button).click(function(e){
+        e.preventDefault();
+        if(x < max_fields){
+            x++;
+            $(wrapper).append('<div><input type="text" name="recipe_prep"/><a href="#" class="delete">Delete</a></div>'); //add input box
+        }
+  else
+  {
+  alert('You Reached the limits')
+  }
+    });
+ 
+    $(wrapper).on("click",".delete", function(e){
+        e.preventDefault(); $(this).parent('div').remove(); x--;
+    })
+});
