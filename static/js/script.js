e361adf258e3df6f59d7e56a01f4e64a4bdaefbe
brianscan14 <brianscanlon14@gmail.com>
2020-01-29 16:08:54 +0000

    renaming and adding new classes for js function so the I can test the adding steps to ingredients function

--- static/js/script.js
+++ static/js/script.js
@@ -4,9 +4,9 @@
 jQuery.noConflict();
 jQuery(document).ready(function($) {
   $('.form-caps').keyup(function(event) {
-    var textBox = event.target;
-    var start = textBox.selectionStart;
-    var end = textBox.selectionEnd;
+    let textBox = event.target;
+    let start = textBox.selectionStart;
+    let end = textBox.selectionEnd;
     textBox.value = textBox.value.charAt(0).toUpperCase() + textBox.value.slice(1).toLowerCase();
     textBox.setSelectionRange(start, end);
   });
@@ -14,25 +14,51 @@ jQuery(document).ready(function($) {
 
 // testing the add form function
 
+
+
 $(document).ready(function() {
-    var max_fields      = 20;
-    var wrapper         = $(".container1");
-    var add_button      = $(".add_form_field");
- 
-    var x = 1;
+    let max_fieldz      = 20;
+    let wrapperer       = $(".contain");
+    let add_buttons     = $(".add_form");
+    let xy = 1;
+
+    $(add_buttons).click(function(e){
+        e.preventDefault();
+        if(xy < max_fieldz){
+            xy++;
+            $(wrapperer).append('<div><input type="text" name="ingredients"/><a href="#" class="del">Delete</a></div>');
+        }
+        else
+        {
+            alert('You Reached the limit of fields!')
+        }
+        });
+        
+            $(wrapperer).on("click",".del", function(e){
+            e.preventDefault(); $(this).parent('div').remove(); xy--;
+        })
+});
+
+
+$(document).ready(function() {
+    let max_fields      = 20;
+    let wrapper         = $(".container1");
+    let add_button      = $(".add_form_field");
+    let x = 1;
+
     $(add_button).click(function(e){
         e.preventDefault();
         if(x < max_fields){
             x++;
             $(wrapper).append('<div><input type="text" name="recipe_prep"/><a href="#" class="delete">Delete</a></div>'); //add input box
         }
-  else
-  {
-  alert('You Reached the limit of fields!')
-  }
-    });
- 
-    $(wrapper).on("click",".delete", function(e){
-        e.preventDefault(); $(this).parent('div').remove(); x--;
-    })
+        else
+        {
+            alert('You Reached the limit of fields!')
+        }
+        });
+        
+            $(wrapper).on("click",".delete", function(e){
+            e.preventDefault(); $(this).parent('div').remove(); x--;
+        })
 });
