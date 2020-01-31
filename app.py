d0f0b7efed7d9b163526ad3f81899823a000b384
brianscan14 <brianscanlon14@gmail.com>
2020-01-31 23:31:17 +0000

    changing page names to conform to html naming practices and adding them to 'pages' folder

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('pages/index.html')
 
 @app.route('/get_recipies')
 def get_recipies():
@@ -25,7 +25,7 @@ def get_recipies():
     total_recipes = mongo.db.recipies.find().skip((curent_page - 1)*6).limit(6)
     num_pages = range(1, int(total_docs / 6) + 2)
 
-    return render_template("recipies.html", recipies=total_recipes, curent_page=curent_page, num_pages=num_pages, total=total_docs)
+    return render_template("pages/recipies.html", recipies=total_recipes, curent_page=curent_page, num_pages=num_pages, total=total_docs)
 
 @app.route('/search_recipe', methods=["GET", "POST"])
 def search_recipe():
@@ -37,19 +37,19 @@ def search_recipe():
             {'ingredients': query}]})
 
     if results.count() > 0:
-        return render_template('search_recipe.html', query=rec_search_query, results=results)
+        return render_template('pages/searchrecipe.html', query=rec_search_query, results=results)
         
     else:
-        return render_template('search_recipe_null.html', query=rec_search_query, results=results)
+        return render_template('pages/searchnull.html', query=rec_search_query, results=results)
 
 @app.route('/recipe_selected/<recipe_id>')
 def recipe_selected(recipe_id):
     recipies = mongo.db.recipies.find_one({'_id': ObjectId(recipe_id)})
-    return render_template('recipe_selected.html', recipe=recipies)
+    return render_template('pages/recipe.html', recipe=recipies)
 
 @app.route('/add_recipe')
 def add_recipe():
-    return render_template('add_recipe.html', recipies=mongo.db.recipies.find())
+    return render_template('pages/addrecipe.html', recipies=mongo.db.recipies.find())
 
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
@@ -66,13 +66,13 @@ def insert_recipe():
         'meal_type': request.form.get('meal_type')
     })
     ret = recipies.find_one({"_id": this_recipe.inserted_id})
-    return render_template('thankyoupage.html', recipe=ret)
+    return render_template('pages/thankyou.html', recipe=ret)
 
 @app.route('/edit_recipe/<recipe_id>', methods=['GET', 'POST'])
 def edit_recipe(recipe_id):
     recipeDB = mongo.db.recipies.find_one({"_id": ObjectId(recipe_id)})
     all_cuisine = mongo.db.recipies.find()
-    return render_template('edit_recipe.html', recipe=recipeDB, recipies=all_cuisine) 
+    return render_template('pages/editrecipe.html', recipe=recipeDB, recipies=all_cuisine) 
 
 @app.route('/update_recipe/<recipe_id>', methods=['GET', 'POST']) 
 def update_recipe(recipe_id): 
@@ -97,7 +97,7 @@ def delete_recipe(recipe_id):
 
 @app.route('/get_cuisines')
 def get_cuisines():
-    return render_template('cuisines.html',
+    return render_template('pages/cuisines.html',
     recipies=mongo.db.recipies.find().distinct("cuisine_name"))
 
 @app.route('/search_cuisines')
@@ -105,7 +105,7 @@ def search_cuisines():
     rec_search_query = request.args['query']
     query = {'$regex': re.compile('.*{}.*'.format(rec_search_query), re.IGNORECASE)}
     results = mongo.db.recipies.find({'cuisine_name': query})
-    return render_template('search_cuisines.html', query=rec_search_query, results=results)
+    return render_template('pages/searchcuisines.html', query=rec_search_query, results=results)
 
 @app.route("/find_meals", methods=['GET', 'POST'])
 def find_meals():
@@ -114,17 +114,17 @@ def find_meals():
         requested_meal_type = request.form.get("meal_type")
         
         recipies = mongo.db.recipies.find({"meal_type": requested_meal_type})
-        return render_template("meal_results.html", recipies=recipies)
+        return render_template("pages/mealresults.html", recipies=recipies)
         
-    return render_template("find_meals.html")
+    return render_template("pages/findmeals.html")
 
 @app.route("/meal_results")
 def meal_results():
-    return render_template('meal_results.html')
+    return render_template('pages/mealresults.html')
 
 @app.route("/thankyoupage")
 def thankyoupage():
-    return render_template('thankyoupage.html')
+    return render_template('pages/thankyou.html')
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
