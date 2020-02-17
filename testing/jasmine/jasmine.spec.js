6fb54b8f28a86ca36bc98f23262d1b6fa4bcf8f1
brianscan14 <brianscanlon14@gmail.com>
2020-02-17 22:51:48 +0000

    adding test to see if string capitalises, remving UI tests

--- testing/jasmine/jasmine.spec.js
+++ testing/jasmine/jasmine.spec.js
@@ -1,4 +1,4 @@
-describe('Testing - ', () => {
+describe('Testing nav overlay open ', () => {
 
     it ('openSearchBar should increase width to 100%', function() {
         $(".content").append("<div id='overlaySearchBar' style='width:10%'></div>");
@@ -8,4 +8,27 @@ describe('Testing - ', () => {
         expect(searchBar.style.width).toEqual('100%')
     });
     
+});
+
+describe('Testing nav overlay close ', () => {
+
+    it ('openSearchBar should decrease width to 0%', function() {
+        $(".content").append("<div id='overlaySearchBar' style='width:100%'></div>");
+        let searchBar = document.getElementById("overlaySearchBar");
+        expect(searchBar.style.width).toEqual('100%')
+        closeSearchBar();
+        expect(searchBar.style.width).toEqual('0%')
+    });
+    
+});
+
+describe('capitalizeString function', () => {
+        
+    it ('capitalizeString should capitalize the first letter of the elements content', function() {
+        $(".content").append('<input id="capitalizeText" value="this should capitalize">');
+        let text = document.getElementById("capitalizeText");
+        capitalizeString('capitalizeText');
+        expect(text.value).toEqual('This should capitalize')
+    });
+
 });
